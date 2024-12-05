59955b3fba33c3016a559b60e05e15d9a60b20bd
Scott Glover <scottgl@gmail.com>
2024-12-04 19:03:54 -0600

    Reorganize indicator tests

--- tests/indicators/talib/test_kama_talib.py
+++ tests/indicators/talib/test_kama_talib.py
@@ -5,23 +5,24 @@ import pandas as pd
 #sys.path.append('./tests')
 sys.path.append('.')
 from cointrader.exchange.TraderSelectExchange import TraderSelectExchange
-from cointrader.indicators.ATR import ATR
+from cointrader.indicators.KAMA import KAMA
 from cointrader.common.Kline import Kline
 from datetime import datetime, timedelta
 #import matplotlib.pyplot as plt
 import argparse
+from ta.momentum import KAMAIndicator
 
 CLIENT_NAME = "cbadv"
 GRANULARITY = 3600
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Plot ADX indicator')
-    parser.add_argument('--ticker', type=str, help='Ticker symbol (ex. BTC-USD)', default='BTC-USD')
+    parser = argparse.ArgumentParser(description='Plot KAMA indicator')
+    parser.add_argument('--ticker', type=str, help='Ticker symbol', default='BTC-USD')
     #parser.add_argument('--granularity', type=int, help='Granularity in seconds', default=3600)
     args = parser.parse_args()
-
     exchange = TraderSelectExchange(CLIENT_NAME).get_exchange()
-    ticker = exchange.info_ticker_join("BTC", "USD")
+    #ticker = exchange.info_ticker_join("BTC", "USD")
+    ticker = args.ticker
     tickers = exchange.info_ticker_names_list()
     if ticker not in tickers:
         print("Ticker not found")
@@ -58,8 +59,8 @@ if __name__ == '__main__':
     kline = Kline()
     kline.set_dict_names(ts='start')
 
-    atr = ATR(period=14)
-    atr_values = []
+    kama = KAMA('KAMA', 10, 2, 30)
+    kama_values = []
 
     opens = []
     closes = []
@@ -70,10 +71,11 @@ if __name__ == '__main__':
 
     for candle in reversed(candles):
         kline.from_dict(candle)
-        result = atr.update(kline)
-        if result is None:
-            result = np.nan
-        atr_values.append(result)
+        result = kama.update(kline)
+        if kama.ready():
+            kama_values.append(result)
+        else :
+            kama_values.append(np.nan)
         opens.append(kline.open)
         closes.append(kline.close)
         highs.append(kline.high)
@@ -94,14 +96,5 @@ data = {
 df = pd.DataFrame(data)
 df.set_index('Date', inplace=True)
 
-atr_plot = mpf.make_addplot(atr_values, panel=1, color='blue', width=1.5)
-
-mpf.plot(
-    df,
-    type='candle',
-    style='charles',
-    title=f'{ticker} {granularity_name} chart with ADX',
-    ylabel='Price',
-    addplot=[atr_plot],
-    panel_ratios=(3, 1),
-)
+kama_indicator = KAMAIndicator(close=df['Close'], window=10, pow1=2, pow2=30, fillna=True)
+print(kama_indicator.kama())
