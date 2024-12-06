c73e8042742a582ed8044777df0ac3067f5b7bc5
Scott Glover <scottgl@gmail.com>
2024-12-05 19:52:41 -0600

    Added trader simulation from CSV data

---
+++ tools/trade_simulate_past_klines_csv.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+import logging
+from coinbase.websocket import WSClient, WebsocketResponse
+#from coinbase.rest import RESTExchange
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+
+import json
+import sys
+import time
+import argparse
+
+try:
+    import cointrader
+except ImportError:
+    sys.path.append('.')
+
+import pandas as pd
+
+from cointrader.exchange.TraderSelectExchange import TraderSelectExchange
+from cointrader.account.AccountSimulate import AccountSimulate
+from cointrader.execute.TradeExecuteSimulate import TraderExecuteSimulate
+from cointrader.market.Market import Market
+from cointrader.trade.MultiTrader import MultiTrader
+from cointrader.trade.TraderConfig import TraderConfig
+from cointrader.common.Kline import Kline
+from cointrader.config import *
+from cointrader.indicators.EMA import EMA
+
+def main(args):
+    name = args.exchange
+    initial_usdt = args.initial_usdt
+
+    exchange = TraderSelectExchange(name).get_exchange()
+
+    market = Market(exchange=exchange)
+    account = AccountSimulate(exchange=exchange, market=market)
+    account.load_symbol_info()
+    account.load_asset_info()
+
+    all_klines = {}
+    symbols = args.symbols.split(',')
+
+    account.update_asset_balance("USDT", available=initial_usdt, hold=0.0)
+
+    ex = TraderExecuteSimulate(exchange=exchange, account=account)
+
+    print(account.get_account_balances())
+    print("Start Total USDT Balance:")
+    print(account.get_total_balance("USDT"))
+
+    tconfig = TraderConfig(path=f'{name}_trader_config.json')
+    if not tconfig.load_config():
+        tconfig.save_config()
+
+    tconfig.set_trade_symbols(symbols)
+
+    mtrader = MultiTrader(account=account, execute=ex, config=tconfig)
+
+    start_ts = int(datetime.fromisoformat(args.start_date).timestamp())
+    if args.end_date == 'now':
+        end_ts = int(datetime.now().timestamp())
+    else:
+        end_ts = int(datetime.fromisoformat(args.end_date).timestamp())
+
+    df = pd.read_csv(args.csv_path)
+
+    # print all unique strings from the Symbols column
+    unique_symbols = df['Symbol'].unique()
+    print("Symbols:", unique_symbols)
+
+    # Convert the Date column to datetime format
+    df['Date'] = pd.to_datetime(df['Date'], format='mixed') #'%Y-%m-%d %H:%M:%S')
+    # Convert the Date column to unix timestamp
+    df['Timestamp'] = df['Date'].apply(lambda x: int(x.timestamp()))
+    # Sort the DataFrame by the Date column
+    df = df.sort_values(by='Date').reset_index(drop=True)
+    # Convert all symbols ending with 'USDT' to '-USDT'
+    df['Symbol'] = df['Symbol'].apply(lambda x: x.replace('USDT', '-USDT'))
+    # Display the first few rows of the DataFrame
+    print(df.head())
+
+    # Filter the DataFrame by the Timestamp column
+    df = df[(df['Timestamp'] >= start_ts) & (df['Timestamp'] <= end_ts)]
+
+    kline = Kline()
+    #kline.set_dict_names(ts='start')
+
+    # iterate through all rows in the DataFrame
+    for index, row in df.iterrows():
+        symbol = row['Symbol']
+        if symbol in symbols:
+            kline_data = {
+                'ts': row['Timestamp'],
+                'open': row['Open'],
+                'high': row['High'],
+                'low': row['Low'],
+                'close': row['Close'],
+                'volume': row['Volume USDT']
+            }
+            #print(kline_data)
+            kline.from_dict(kline_data)
+            kline.symbol = symbol
+            mtrader.market_update(kline)
+
+    #last_prices = {}
+    #for symbol in symbols:
+    #    last_prices[symbol] = all_klines[symbol][-1]['close']
+
+    print(account.get_account_balances())
+    #print("\nFinal Total USDT Balance:")
+    #print(account.get_total_balance("USDT", prices=last_prices))
+
+    print("\nRemaining open positions:")
+    for symbol in symbols:
+        position = mtrader.position_count(symbol)
+        print(f"{symbol} position_count: {position}")
+
+    print("\nNet profit on closed positions:")
+    for symbol in symbols:
+        profit = mtrader.net_profit_percent(symbol)
+        print(f"{symbol} net profit: {profit:.2f}%")
+
+    buys = {}
+    sells = {}
+
+    #print("\nBuys and Sells:")
+    for symbol in symbols:
+        buys[symbol] = mtrader.buys(symbol)
+        #print(f"{symbol} buys: {buys}")
+        sells[symbol] = mtrader.sells(symbol)
+        #print(f"{symbol} sells: {sells}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Trade simulation with past klines.')
+    parser.add_argument('--initial_usdt', type=float, default=10000.0, help='Initial USDT amount for simulation')
+    parser.add_argument('--exchange', type=str, default="cbadv", help='Account to use for simulation')
+    parser.add_argument('--granularity', type=int, default=3600, help='Granularity of klines')
+    parser.add_argument('--csv_path', type=str, default='data/crypto_hourly_data/cryptotoken_full_binance_1h.csv', help='Path to the CSV file')
+    parser.add_argument('--symbols', type=str, default='BTC-USDT,ETH-USDT,SOL-USDT', help='Comma separated list of symbols')
+    parser.add_argument('--start_date', type=str, default='2020-08-11 06:00:00', help='Start date for klines')
+    parser.add_argument('--end_date', type=str, default='2023-10-19 23:00:00', help='End date for klines')
+    args = parser.parse_args()
+    main(args)
